738c2150a1e283da641f2b2f5fe4d656f9d07528
femarko <fenstermark7@gmail.com>
2025-04-08 01:09:41 +0200

    feat: src/shortener_app/entrypoints/fastapi_app/routs.py - get_original_url() added.

--- src/shortener_app/entrypoints/fastapi_app/routs.py
+++ src/shortener_app/entrypoints/fastapi_app/routs.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-from decimal import Decimal
-from typing import cast
 
 from src.shortener_app.entrypoints.fastapi_app.schemas import URL
 from src.shortener_app.sevice_layer import app_manager, unit_of_work
@@ -12,15 +10,13 @@ url_shotter_routs = APIRouter()
 
 @url_shotter_routs.post("/cut_url")
 def cut_url(original_url: URL) -> dict[str, int | str]:
-    result = app_manager.cut_url_and_save(original_url=original_url.url,
-                                          uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker))
-    return result
+    return app_manager.cut_url_and_save(
+        original_url=original_url.url, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
+    )
 
 
-@url_shotter_routs.get("/get_info_from_db")
-def get_info_from_db(number: int = 20,
-                     page: int = 1,
-                     per_page: int = 5) -> dict[str, int | list[dict[str, str |int | Decimal]]]:
-    return app_manager.get_info_from_db(
-        number=number, page=page, per_page=per_page, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
+@url_shotter_routs.get("/original_url/{id}")
+def get_original_url(id: int) -> str:
+    return app_manager.get_original_url(
+        urls_instance_id=id, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
     )
