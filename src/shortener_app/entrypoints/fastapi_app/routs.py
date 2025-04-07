50696e83385dacf4c97c898e52b5fd887ac1cb37
femarko <fenstermark7@gmail.com>
2025-04-08 01:46:04 +0200

    refact: src/shortener_app/entrypoints/fastapi_app/routs.py - response codes changed; response of get_original_url() modified.

--- src/shortener_app/entrypoints/fastapi_app/routs.py
+++ src/shortener_app/entrypoints/fastapi_app/routs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 from src.shortener_app.entrypoints.fastapi_app.schemas import URL
 from src.shortener_app.sevice_layer import app_manager, unit_of_work
@@ -8,15 +8,16 @@ from src.shortener_app.orm_tool.sql_aclchemy_wrapper import orm_conf
 url_shotter_routs = APIRouter()
 
 
-@url_shotter_routs.post("/cut_url")
+@url_shotter_routs.post("/", status_code=201)
 def cut_url(original_url: URL) -> dict[str, int | str]:
     return app_manager.cut_url_and_save(
         original_url=original_url.url, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
     )
 
 
-@url_shotter_routs.get("/original_url/{id}")
-def get_original_url(id: int) -> str:
-    return app_manager.get_original_url(
-        urls_instance_id=id, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
+@url_shotter_routs.get("/{shorten_url_id}", status_code=307)
+def get_original_url(shorten_url_id: int) -> Response:
+    data = app_manager.get_original_url(
+        urls_instance_id=shorten_url_id, uow=unit_of_work.UnitOfWork(session_maker=orm_conf.session_maker)
     )
+    return Response(status_code=307, headers={"Location": data})
