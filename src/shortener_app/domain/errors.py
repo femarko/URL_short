90b16e6a31a6539b74a4d2749dfd2d3320931243
femarko <fenstermark7@gmail.com>
2025-04-13 18:22:08 +0200

    feat: src/shortener_app/domain/errors.py - DBError added.

--- src/shortener_app/domain/errors.py
+++ src/shortener_app/domain/errors.py
@@ -28,10 +28,16 @@ class CurrentUserError(Exception):
 
 class AlreadyExistsError(Exception):
     def __init__(self, message_prefix: Optional[str] = ""):
-        self.base_message = "with the provided params already existsts."
+        self.base_message = " with the provided params already existsts."
         self.message = message_prefix + self.base_message
 
 
 class UnexpectedError(Exception):
-    def __init__(self, message: Optional[str] = "Unexpected server error."):
+    def __init__(self, message_postfix: Optional[str] = ""):
+        self.base_message = "Unexpected server error: "
+        self.message = self.base_message + message_postfix
+
+
+class DBError(Exception):
+    def __init__(self, message):
         self.message = message
