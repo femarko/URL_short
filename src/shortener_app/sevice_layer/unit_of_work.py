3263a1ecff7079dabbeaa2584e3f0bb33578c198
femarko <fenstermark7@gmail.com>
2025-04-13 19:17:32 +0200

    refact: src/shortener_app/sevice_layer/unit_of_work.py - sqlalchemy staff replaced with orm_conf attrs, typing added to __aenter__().

--- src/shortener_app/sevice_layer/unit_of_work.py
+++ src/shortener_app/sevice_layer/unit_of_work.py
@@ -1,6 +1,4 @@
-from typing import Callable, Protocol, Type
-
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Callable
 
 import src.shortener_app.domain.errors as domain_errors
 from src.shortener_app.orm_tool.sql_aclchemy_wrapper import orm_conf
@@ -8,12 +6,12 @@ from src.shortener_app.repository.repository import URLRepository, RepoProto
 
 
 class UnitOfWork:
-    def __init__(self, session_maker: Callable[[], AsyncSession] = orm_conf.session_maker):
+    def __init__(self, session_maker: Callable[..., orm_conf.asyncsession] = orm_conf.session_maker):
         self.session_maker = session_maker
 
     async def __aenter__(self) -> "UnitOfWork":
-        self.session = self.session_maker()
-        self.url_repo = URLRepository(session=self.session)
+        self.session: orm_conf.asyncsession = self.session_maker()
+        self.url_repo: RepoProto = URLRepository(session=self.session)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
